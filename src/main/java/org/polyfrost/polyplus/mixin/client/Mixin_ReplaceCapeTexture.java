faabc766216166a54fb3530f0eddfcc9e1bdc8a3
Deftu <me@deftu.dev>
2025-11-09 10:14:48 +0200

    Update Mixin_ReplaceCapeTexture.java

--- src/main/java/org/polyfrost/polyplus/mixin/client/Mixin_ReplaceCapeTexture.java
+++ src/main/java/org/polyfrost/polyplus/mixin/client/Mixin_ReplaceCapeTexture.java
@@ -11,6 +11,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+/**
+ * Replaces the location with which players' capes are rendered with our own custom cape location should the user have one equipped.
+ *
+ * @author subat0mic
+ * @since 1.0.0
+ */
 @Mixin(NetworkPlayerInfo.class)
 public class Mixin_ReplaceCapeTexture {
     @Shadow @Final private GameProfile gameProfile;
